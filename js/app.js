ce8d746387a26e5b29ef8c90a867e34b2ce495b9
jasainformatika <dwiranggaoktaz@gmail.com>
2023-09-17 00:36:32 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,13 +1,19 @@
+// app.js
 
 document.addEventListener('DOMContentLoaded', function () {
     const themeSwitch = document.getElementById('theme-switch');
 
+    // Fungsi untuk mengubah tema
     function toggleTheme() {
         if (themeSwitch.checked) {
+            // Aktifkan dark mode
             document.body.classList.add('dark-mode');
         } else {
+            // Nonaktifkan dark mode
             document.body.classList.remove('dark-mode');
         }
     }
+
+    // Tambahkan event listener ke checkbox
     themeSwitch.addEventListener('change', toggleTheme);
 });
