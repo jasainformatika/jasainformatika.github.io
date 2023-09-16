a1a4b54439583d09ac6819180506ecd287993faf
jasainformatika <dwiranggaoktaz@gmail.com>
2023-09-16 22:17:53 +0700

    1.0.1

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 setTimeout(function () {
   var splashScreen = document.querySelector("#splash");
   splashScreen.parentNode.removeChild(splashScreen);
-  window.location.href = "/home.html"; // Ganti dengan nama file halaman index Anda jika berbeda
-}, 4000);
+  window.location.href = "/home.html";
+}, 3000);
