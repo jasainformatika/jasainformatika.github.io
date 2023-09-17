f3ed31f9df0ab700e456a8f015126036eca07506
jasainformatika <dwiranggaoktaz@gmail.com>
2023-09-17 15:27:09 +0700

    1.0.5

--- js/app.js
+++ js/app.js
@@ -20,3 +20,4 @@ document.addEventListener('DOMContentLoaded', function () {
     themeSwitch.addEventListener('change', toggleTheme);
     toggleButton.addEventListener('change', toggleTheme);
 });
+
